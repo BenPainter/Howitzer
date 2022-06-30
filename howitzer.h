9ce87b68006e473fd8ad0335fc97225467262e3e
starb520 <97459015+starb520@users.noreply.github.com>
2022-06-30 12:09:27 -0600

    added howitzer.h and howitzer.cpp

--- howitzer.h
+++ howitzer.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "position.h"
+#include "uiDraw.h"
+
+using namespace std;
+/*****************************************************
+* Howitzer
+******************************************************/
+
 class Howitzer
 {
+private:
+   Position pt;
+   double angle;
+   bool fired;
+   double age;
+
+public:
+   Howitzer() { reset(); };
+   Howitzer(Position pt) { this->pt = pt; };
+   void input();
+   bool isFired();
+   Position getPT();
+   double getAngle();
+   Position setPT();
+   double setAngle();
+   void draw(ogstream& gout);
+   double getAge();
+   void reset();
+
 };
 
