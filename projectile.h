aa05b256b99f5a7a242f086ea8e5f4ad2853c132
Ben Painter <ben4painter@gmail.com>
2022-06-30 12:10:37 -0600

    projectile blanks

--- projectile.h
+++ projectile.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "position.h"
 #include "velocity.h"
+#include "uiDraw.h"
 #include <vector>
 
 using namespace std;
@@ -12,6 +13,23 @@ private:
    vector<Position> tail;
    enum Status { ALIVE, HIT, MISS };
    Status status;
+   void updateTail();
+
+public:
+   Projectile() { reset(); };
+   Projectile(Position pt) { this->pt = pt;  };
+   void draw(ogstream& gout);
+
+   bool isAlive();
+   bool isHit();
+   bool isMiss();
+
+   void setPT(Position pt);
+   void update(Acceleration accel);
+   void reset();
+
+   void hitTargert();
+   void missTarget();
 
 };
 
