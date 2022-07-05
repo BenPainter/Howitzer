2a87bff770fd85d8dcf9f7d04db918c7e3a4088d
Ben Painter <ben4painter@gmail.com>
2022-07-05 11:10:50 -0600

    display

--- Lab07.cpp
+++ Lab07.cpp
@@ -142,22 +142,21 @@ int WINAPI wWinMain(
 int main(int argc, char** argv)
 #endif // !_WIN32
 {
-   //// Initialize OpenGL
-   //Position ptUpperRight;
-   //ptUpperRight.setPixelsX(700.0);
-   //ptUpperRight.setPixelsY(500.0);
-   //Position().setZoom(40.0 /* 42 meters equals 1 pixel */);
-   //Interface ui(0, NULL,
-   //   "Game",   /* name on the window */
-   //   ptUpperRight);
+   // Initialize OpenGL
+   Position ptUpperRight;
+   ptUpperRight.setPixelsX(700.0);
+   ptUpperRight.setPixelsY(500.0);
+   Position().setZoom(40.0 /* 42 meters equals 1 pixel */);
+   Interface ui(0, NULL,
+      "Game",   /* name on the window */
+      ptUpperRight);
 
-   //// Initialize the game
-   //Game game(ptUpperRight);
+   // Initialize the game
+   Game game(ptUpperRight);
 
-   //// set everything into action
-   // ui.run(callBack, &game);
+   // set everything into action
+    ui.run(callBack, &game);
 
-   testRunner();
 
    return 0;
 }
